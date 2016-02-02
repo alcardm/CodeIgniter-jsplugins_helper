f92b5a8f6957050388beb091d3c9d10c74527dbf
alcardm <alcardm@gmail.com>
2016-02-02 18:21:43 -0500

    Update jsplugins_helper.php
    Now with render

--- application/helpers/jsplugins_helper.php
+++ application/helpers/jsplugins_helper.php
@@ -6,15 +6,16 @@
  * @category Helper
  * @author Alberto Cardenas
  * @link http://albertocardenas.co
- * @version 1.0
+ * @version 1.5
  */
 
 if ( ! function_exists('plugin_js'))
 {
 	
-	function plugin_js($library = 'jquery')
+	function plugin_js($library = 'jquery', $render = false)
 	{
-		static $js;
+		$CI =& get_instance();
+                static $js;
 
 		if ( ! is_array($js))
 		{
@@ -22,11 +23,7 @@ if ( ! function_exists('plugin_js'))
 			{
 				include(APPPATH.'config/jsplugins.php');
 			}
-
-			if (file_exists(APPPATH.'config/'.ENVIRONMENT.'/jsplugins.php'))
-			{
-				include(APPPATH.'config/'.ENVIRONMENT.'/jsplugins.php');
-			}
+                        
 			if (empty($_js) OR ! is_array($_js))
 			{
 				$js = array();
@@ -35,15 +32,24 @@ if ( ! function_exists('plugin_js'))
 
 			$js = $_js;
 		}
-		return isset($js[$library]) ? '<script src="' . $js[$library] . '"></script>' : FALSE;
+                if( $render == true){
+                    return '<script src="' . $CI->config->site_url($library) . '"></script>';
+                }
+                if (preg_match('#^https://.*#s', $js[$library])){
+                    $string = $js[$library];
+                } else {
+                    $string = $CI->config->site_url($js[$library]);
+                }
+		return isset($js[$library]) ? '<script src="' . $string . '"></script>' : false;
 	}
 }
 
 if ( ! function_exists('plugin_css'))
 {
 	
-	function plugin_css($style = 'style')
+	function plugin_css($style = 'style', $render = false)
 	{
+                $CI =& get_instance();
 		static $css;
 
 		if ( ! is_array($css))
@@ -52,11 +58,7 @@ if ( ! function_exists('plugin_css'))
 			{
 				include(APPPATH.'config/jsplugins.php');
 			}
-
-			if (file_exists(APPPATH.'config/'.ENVIRONMENT.'/jsplugins.php'))
-			{
-				include(APPPATH.'config/'.ENVIRONMENT.'/jsplugins.php');
-			}
+                        
 			if (empty($_js) OR ! is_array($_js))
 			{
 				$js = array();
@@ -65,6 +67,14 @@ if ( ! function_exists('plugin_css'))
 
 			$css = $_css;
 		}
-		return isset($css[$style]) ? '<link rel="stylesheet" href="' . $css[$style] . '">' : FALSE;
+                if( $render == true){
+                    return '<link rel="stylesheet" href="' . $CI->config->site_url($style) . '">';
+                }
+                if (preg_match('#^https://.*#s', $css[$style])){
+                    $string = $css[$style];
+                } else {
+                    $string = $CI->config->site_url($css[$style]);
+                }
+		return isset($css[$style]) ? '<link rel="stylesheet" href="' . $string . '">' : FALSE;
 	}
 }
